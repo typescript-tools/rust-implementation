6aefc969e3ceb430178388f71a197a06440482a5
Eric Crosson <eric.s.crosson@utexas.edu>
2021-11-14 12:08:06 -0600

    fix: do not write null values for nonexistent keys

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
@@ -8,7 +8,7 @@ use clap::Parser;
 
 use glob::glob;
 
-use serde::{Deserialize, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
 use serde_json;
 
 #[derive(Parser)]
@@ -28,25 +28,9 @@ struct LernaManifest {
 struct PackageManifest {
     name: String,
     version: String,
-    // #[serde(serialize_with = "ordered_map")]
-    dependencies: Option<HashMap<String, String>>,
-    // #[serde(serialize_with = "ordered_map")]
-    dev_dependencies: Option<HashMap<String, String>>,
-    // #[serde(serialize_with = "ordered_map")]
-    optional_dependencies: Option<HashMap<String, String>>,
-    // #[serde(serialize_with = "ordered_map")]
-    peer_dependencies: Option<HashMap<String, String>>,
     #[serde(flatten)]
-    extra: HashMap<String, serde_json::Value>,
+    extra_fields: HashMap<String, serde_json::Value>,
 }
-
-// fn ordered_map<S>(value: &HashMap<String, String>, serializer: S) -> Result<S::Ok, S::Error>
-// where
-//     S: Serializer,
-// {
-//     let ordered: BTreeMap<_, _> = value.iter().collect();
-//     ordered.serialize(serializer)
-// }
 
 fn read_lerna_manifest(root: &Path) -> Result<LernaManifest, Box<dyn Error>> {
     let file = File::open(root.join("lerna.json"))?;
@@ -94,31 +78,31 @@ fn pin_version_numbers_in_internal_packages(
 ) -> HashMap<String, PackageManifest> {
 
     for package_manifest in internal_packages.values_mut() {
-        if let Some(deps) = package_manifest.dependencies.as_mut() {
+        if let Some(deps) = package_manifest.extra_fields.get_mut("dependencies").and_then(|v| serde_json::Value::as_object_mut(v)) {
             for (package, version) in deps.iter_mut() {
                 if let Some(internal_version) = version_by_name.get(package) {
-                    *version = internal_version.to_string();
+                    *version = serde_json::Value::String(internal_version.to_string());
                 }
             }
         }
-        if let Some(deps) = package_manifest.dev_dependencies.as_mut() {
+        if let Some(deps) = package_manifest.extra_fields.get_mut("devDependencies").and_then(|v| serde_json::Value::as_object_mut(v)) {
             for (package, version) in deps.iter_mut() {
                 if let Some(internal_version) = version_by_name.get(package) {
-                    *version = internal_version.to_string();
+                    *version = serde_json::Value::String(internal_version.to_string());
                 }
             }
         }
-        if let Some(deps) = package_manifest.optional_dependencies.as_mut() {
+        if let Some(deps) = package_manifest.extra_fields.get_mut("optionalDependencies").and_then(|v| serde_json::Value::as_object_mut(v)) {
             for (package, version) in deps.iter_mut() {
                 if let Some(internal_version) = version_by_name.get(package) {
-                    *version = internal_version.to_string();
+                    *version = serde_json::Value::String(internal_version.to_string());
                 }
             }
         }
-        if let Some(deps) = package_manifest.peer_dependencies.as_mut() {
+        if let Some(deps) = package_manifest.extra_fields.get_mut("peerDependencies").and_then(|v| serde_json::Value::as_object_mut(v)) {
             for (package, version) in deps.iter_mut() {
                 if let Some(internal_version) = version_by_name.get(package) {
-                    *version = internal_version.to_string();
+                    *version = serde_json::Value::String(internal_version.to_string());
                 }
             }
         }
