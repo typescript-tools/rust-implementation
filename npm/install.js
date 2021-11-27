ae0bd522322ae4d984a2326c6e6af6aaaa6bfdf2
Eric Crosson <eric.s.crosson@utexas.edu>
2021-11-27 08:40:52 -0600

    fix: include a default bin/monorepo script
    This dummy acts as a target for `npm` to link into `node_modules/.bin`
    and `chmod` when run in such a way that the `monorepo` executable has
    not been downloaded yet.
    
    Closes #19

--- npm/install.js
+++ npm/install.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const tmp = require('tmp')
 const getBinary = require('./get-binary')
 
 const binaryName = 'monorepo'
@@ -7,16 +8,24 @@ const repoDir = path.dirname(__dirname)
 const downloadDir = path.resolve(repoDir, 'bin')
 const scopedPackageDir = path.dirname(repoDir)
 const nodeModulesDir = path.dirname(scopedPackageDir)
+const nodeModulesBinDir = path.resolve(nodeModulesDir, '.bin')
 
-// Link the downloaded binary into the node_modules/.bin directory
+// Link the downloaded binary into the node_modules/.bin directory.
+// Inspiration from https://github.com/evanw/esbuild/releases/tag/v0.13.4
 const linkBinaryIntoBin = () => {
+  const tempPath = tmp.tmpNameSync({ tmpdir: nodeModulesBinDir })
   const existingPath = path.resolve(downloadDir, binaryName)
-  const desiredPath = path.resolve(nodeModulesDir, '.bin', binaryName)
+  const desiredPath = path.resolve(nodeModulesBinDir, binaryName)
 
-  // Only symlink the path when it does not yet exist
-  if (!fs.existsSync(desiredPath)) {
-    fs.linkSync(existingPath, desiredPath)
-  }
+  // First, link the binary with a temporary file. If this fails and throw an error,
+  // then we'll end up doing nothing. This uses a hard link to avoid taking up
+  // additional space on the file system.
+  fs.linkSync(existingPath, tempPath)
+
+  // Then use rename to atomically replace the target file with the temporary file.
+  // If this fails and throws an error, we'll end up leaving the temporary file there,
+  // which is harmless.
+  fs.renameSync(tempPath, desiredPath)
 }
 
 // Downloaded binary to /bin/{ name }
