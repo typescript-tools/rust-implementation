ac17622746b684186503aaca3b32685a059e0da5
Eric Crosson <eric.s.crosson@utexas.edu>
2023-04-30 12:06:27 -0500

    fix: provide path to io::FromFileError

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![forbid(unsafe_code)]
 #![deny(missing_debug_implementations)]
 
-mod io;
 mod out_of_date_project_references;
 mod unpinned_dependencies;
 
 pub mod configuration_file;
+pub mod io;
 pub mod link;
 pub mod lint;
 pub mod make_depend;
