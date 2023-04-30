f83e6ac4ae16d73f32669330ac388dc23b26330d
Eric Crosson <eric.s.crosson@utexas.edu>
2023-04-29 20:16:05 -0500

    test: use @typescript-tools scope for example package names
    This avoids renovate from seeing these as real deps, like in #357

--- tests/lint.rs
+++ tests/lint.rs
@@ -10,5 +10,8 @@ fn lint_happy_path_should_not_error() -> Result<(), LintError> {
 #[test]
 fn lint_should_error_when_multiple_version_of_an_external_dependency_are_used() {
     let root = "test_data/external_dependency_multiple_versions";
-    assert!(typescript_tools::lint::lint_dependency_version(root, &["external"]).is_err());
+    assert!(
+        typescript_tools::lint::lint_dependency_version(root, &["@typescript-tools/external"])
+            .is_err()
+    );
 }
