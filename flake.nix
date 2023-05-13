460686715049faf22cd4091524dc7fb7a93631ba
Eric Crosson <eric.s.crosson@utexas.edu>
2023-05-12 20:20:53 -0500

    build(nix): refactor dev only flake inputs into subflake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -17,92 +11,20 @@
     self,
     nixpkgs,
     crane,
-    flake-utils,
-    pre-commit-hooks,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      craneLib = crane.lib.${system};
-
-      testDataFilter = path: type: builtins.match ".*/test_data.*" path != null;
-      templateFilter = path: _type: builtins.baseNameOf path == "makefile";
-      templateOrCargoOrTestFiles = path: type:
-        (templateFilter path type) || (craneLib.filterCargoSources path type) || (testDataFilter path type);
-
-      # Common derivation arguments used for all builds
-      commonArgs = {
-        src = pkgs.lib.cleanSourceWith {
-          src = ./.;
-          filter = templateOrCargoOrTestFiles;
-        };
-      };
-
-      # Build *just* the cargo dependencies, so we can reuse
-      # all of that work (e.g. via cachix) when running in CI
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-      # Run clippy (and deny all warnings) on the crate source,
-      # resuing the dependency artifacts (e.g. from build scripts or
-      # proc-macros) from above.
-      #
-      # Note that this is done as a separate derivation so it
-      # does not impact building just the crate by itself.
-      myCrateClippy = craneLib.cargoClippy (commonArgs
-        // {
-          # Again we apply some extra arguments only to this derivation
-          # and not every where else. In this case we add some clippy flags
-          inherit cargoArtifacts;
-          cargoClippyExtraArgs = "-- --deny warnings";
-        });
-
-      # Next, we want to run the tests and collect code-coverage, _but only if
-      # the clippy checks pass_ so we do not waste any extra cycles.
-      myCrateCoverage = craneLib.cargoNextest (commonArgs
-        // {
-          cargoArtifacts = myCrateClippy;
-        });
-
-      # Build the actual crate itself, reusing the dependency
-      # artifacts from above.
-      myCrate = craneLib.buildPackage (commonArgs
-        // {
-          inherit cargoArtifacts;
-        });
-
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          actionlint.enable = true;
-          alejandra.enable = true;
-          prettier.enable = true;
-        };
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    packages = forEachSystem (system: let
+      craneDerivations = nixpkgs.legacyPackages.${system}.callPackage ./nix/default.nix {
+        inherit crane;
       };
     in {
-      packages.default = myCrate;
-      checks = {
-        inherit
-          myCrate
-          myCrateClippy
-          myCrateCoverage
-          pre-commit-check
-          ;
-      };
-      devShells = {
-        default = nixpkgs.legacyPackages.${system}.mkShell {
-          nativeBuildInputs = [
-            pkgs.cargo
-            pkgs.clippy
-            pkgs.nodejs
-            pkgs.rustc
-            pkgs.rust-analyzer
-          ];
-
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-        };
-      };
+      default = craneDerivations.myCrate;
     });
+  };
 }
